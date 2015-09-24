2190ac4dd44721b5cf757d07ada3575ff976df95
jscottchapman <scott8675309@gmail.com>
2015-09-24 19:57:21 -0400

    added enemies and enemy options

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,42 @@
+Create a constructor --prototype for the enemies and something to generate them.  a prototype that does an attack  name health and damage, winning tag line.
+
+
+function Enemy(options){
+  this.name = options.name;
+  this.greeting = options.greeting;
+  this.damage = options.damage;
+  this.brag = options.brag;
+}
+
+var wu-tang-jake = new Enemy({
+  name = "Wu-Tang Jake",
+  greeting="May the Wu be with you.",
+  damage = 3,
+  brag = "Wu Tang Jake ain't nothing to mess with!"
+});
+
+
+var pithy-jake = new Enemy({
+  name = "Pithy Jake",
+  greeting = "No judgements here",
+  damage = 2,
+  brag = "I told you to follow your heart...not lose your head!"
+});
+
+var sedated-jake = new Enemy({
+  name = "Sedated Jake"
+  greeting = "Sorry I'm late, I just woke up.  Prepare to die."
+  damage = 1,
+  brag = "I can't believe I got out of bed for this."
+});
+
+
+
+
+
+
+
+
 // var models = require('models');
 // var views = require('views');
 //
