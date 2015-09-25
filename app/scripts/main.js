779440723828dd07e74f5958caf8b0f6cb6997af
Aaron Emery <aemery14@gmail.com>
2015-09-25 10:27:11 -0400

    Fixed syntax errors, merged formatting

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,60 +1,3 @@
-//Create a constructor --prototype for the enemies and something to generate //them.  a prototype that does an attack  name health and damage, winning tag //line.
-
-// Pick a Random enemy using a the damage value and a random number generator
-//var enemy-array =[sedated-jake, pithy-jake, wu-tang-jake]
-var pick-enemy = _.filter([sedated-jake, pithy-jake, wu-tang-jake],
-    function(person){
-      return person _.random(1, 3) === this.damage;
-});
-
-
-
-
-function Enemy(options){
-  this.name = options.name;
-  this.greeting = options.greeting;
-  this.damage = options.damage;
-  this.brag = options.brag;
-}
-
-
-var wu-tang-jake = new Enemy({
-  name = "Wu-Tang Jake",
-  greeting="May the Wu be with you.",
-  damage = 3,
-  brag = "Wu Tang Jake ain't nothing to mess with!",
-  currentHealth = ,
-  maxHealth = ,
-  currentPower = ,
-  startingPower = ,
-});
-
-
-var pithy-jake = new Enemy({
-  name = "Pithy Jake",
-  greeting = "No judgements here",
-  damage = 2,
-  brag = "I told you to follow your heart...not lose your head!"
-  currentHealth = ,
-  maxHealth = ,
-  currentPower = ,
-  startingPower = ,
-});
-
-var sedated-jake = new Enemy({
-  name = "Sedated Jake"
-  greeting = "Sorry I'm late, I just woke up.  Prepare to die."
-  damage = 1,
-  brag = "I can't believe I got out of bed for this."
-  currentHealth = ,
-  maxHealth = ,
-  currentPower = ,
-  startingPower = ,
-});
-
-
-
-
 var models = require('models');
 var views = require('views');
 
@@ -78,10 +21,52 @@ var mason = new Player('Mason', 30, 30, 6, 6);
 var practice = new Player('target', 100, 100, 1, 1);
 var test = new Player('test', 100, 100, 1, 1);
 
+
 /**********
 Enemy Instances
 **********/
 
+// Pick a Random enemy using a the damage value and a random number generator
+//var enemy-array =[sedated-jake, pithy-jake, wu-tang-jake]
+
+var pick_enemy = _.filter([sedated-jake, pithy-jake, wu-tang-jake],
+    function(person){
+      return _.random(1, 3) === this.damage;
+});
+
+var wu_tang_jake = new Enemy({
+  name: "Wu-Tang Jake",
+  greeting: "May the Wu be with you.",
+  damage: 3,
+  brag: "Wu Tang Jake ain't nothing to mess with!",
+  currentHealth: "",
+  maxHealth: "",
+  currentPower: "",
+  startingPower: "",
+});
+
+
+var pithy_jake = new Enemy({
+  name: "Pithy Jake",
+  greeting: "No judgements here",
+  damage: 2,
+  brag: "I told you to follow your heart...not lose your head!",
+  currentHealth: "",
+  maxHealth: "",
+  currentPower: "",
+  startingPower: "",
+});
+
+var sedated_jake = new Enemy({
+  name: "Sedated Jake",
+  greeting: "Sorry I'm late, I just woke up.  Prepare to die.",
+  damage: 1,
+  brag: "I can't believe I got out of bed for this.",
+  currentHealth: "",
+  maxHealth: "",
+  currentPower: "",
+  startingPower: "",
+});
 
 
 
@@ -98,6 +83,12 @@ function Player(name, currentHealth, maxHealth, currentPower, startingPower) {
   this.startingPower = startingPower || 3;
 }
 
+function Enemy(options){
+  this.name = options.name;
+  this.greeting = options.greeting;
+  this.damage = options.damage;
+  this.brag = options.brag;
+}
 
 
 
